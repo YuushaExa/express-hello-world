24fa698ee9cd033ef95b898ffe53e102bb96a0b7
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2024-03-11 23:52:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,3 @@ app.get("/", (req, res) => {
 const server = app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
